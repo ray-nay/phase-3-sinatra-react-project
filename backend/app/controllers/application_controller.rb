2971ebab62b4a90667dd087004311e173450eb27
Renee Mumbi <1723renee@gmail.com>
2023-03-01 21:48:26 +0300

    Create patch route for houses

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,10 +2,28 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
+  # All routes for houses
+
+  # get all houses
   get '/houses' do
    houses= House.all
 
    houses.to_json
   end
 
+
+  # edit houses
+  patch '/houses/:id' do 
+    house = House.find(params[:id])
+    house.update(
+      body: params[:body]
+    )
+    house.to_json
+  end 
+
+  # get house by id
+  get '/houses/:id' do
+    house = House.find(params[:id])
+    house.to_json
+  end
 end

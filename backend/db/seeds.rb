183fe3f5eb000c10cec3b82eb9cbb3c06f52a95c
Renee Mumbi <1723renee@gmail.com>
2023-03-01 18:04:45 +0300

    Add seed data for users

---
+++ backend/db/seeds.rb
@@ -0,0 +1,27 @@
+puts "🌱 Seeding begins..."
+
+# Seed your database here
+puts "Creating users..."
+user1 = User.create(name: Faker::Name.name)
+user2 = User.create(name: Faker::Name.name)
+user3 = User.create(name: Faker::Name.name)
+
+
+user4 = User.create(name: Faker::Name.name)
+user5 = User.create(name: Faker::Name.name)
+user6 = User.create(name: Faker::Name.name)
+
+user7 = User.create(name: Faker::Name.name)
+user8 = User.create(name: Faker::Name.name)
+user9 = User.create(name: Faker::Name.name)
+
+user10 = User.create(name: Faker::Name.name)
+user11 = User.create(name: Faker::Name.name)
+user12 = User.create(name: Faker::Name.name)
+
+
+
+puts "✅ Seeded users!"
+
+puts "Creating houses"
+
